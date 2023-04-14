c1c4489646c2c3247b5a48b8a1e9e909853c4975
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-04-14 17:38:48 +0100

    Implement homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from '@/components/Layout'
+import Home from '@/pages/Index'
 import Product from '@/pages/product/Index'
 
 const router = createBrowserRouter([
@@ -7,6 +8,10 @@ const router = createBrowserRouter([
     path: '/',
     element: <Layout />,
     children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
       {
         path: '/product/',
         element: <Product />,
